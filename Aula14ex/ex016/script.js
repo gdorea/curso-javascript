119b98c61a818d37a9bddc9d32b1a7781f60ee9c
Gerzianni Dórea <gerzii@hotmail.com>
2023-06-03 16:38:19 -0300

    exercícios aula14

---
+++ Aula14ex/ex016/script.js
@@ -0,0 +1,28 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('ERRO! Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} `
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c}`
+            }
+        }
+    }
+}
